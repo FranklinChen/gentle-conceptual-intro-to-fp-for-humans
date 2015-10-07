3c38166eefee8ce78af410a40d41ecbb0ec864cd
Franklin Chen <franklinchen@franklinchen.com>
2015-10-07 17:53:48 -0400

    Forgot to illustrate refactoring.

--- mapGreeters.js
+++ mapGreeters.js
@@ -9,3 +9,6 @@ let myGreeters = [greeter("Hello"), greeter("Howdy")];
 // ["Hello, Franklin", "Howdy, Franklin"]
 let myGreetings =
         myGreeters.map(greeter => greeter("Franklin"));
+
+// Separate IO from pure computation.
+myGreetings.forEach(string => console.log(string));
